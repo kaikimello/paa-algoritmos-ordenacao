084077c1d8b0788be7230709882d3f27da84ecbf
Kaiki Mello dos Santos <kaiki.mello@gmail.com>
2021-12-08 23:29:47 -0300

    Modificações no Main

--- src/GerarOrdenarArquivoMain.java
+++ src/GerarOrdenarArquivoMain.java
@@ -11,7 +11,7 @@ public class GerarOrdenarArquivoMain {
         //GerarArquivo.numerosOrdemCrescente(104857600);
 
         //GerarArquivo.numerosOrdemDecrescente(102300);
-/*
+
         long tempoInicialInsertion = System.currentTimeMillis();
 
         InsertionSort ordenarArquivo = new InsertionSort("numerosDesordenados104857600.txt");
@@ -24,7 +24,6 @@ public class GerarOrdenarArquivoMain {
         System.out.println("O tempo de Execução da função Insertion Sort é: " + tempoExecucaoInsertion);
 
         long tempoInicialMergeSort = System.currentTimeMillis();
-
         MergeSort ordenarArquivoMerge = new MergeSort("numerosDesordenados104857600.txt");
         ordenarArquivoMerge.ordenar();
 
@@ -33,16 +32,16 @@ public class GerarOrdenarArquivoMain {
         String tempoExecucaoMerge = retornaTempo(milliMergeTempo);
 
 
-        System.out.println("O tempo de Execução da função Merge Sort é: " + tempoExecucaoMerge);*/
+        System.out.println("O tempo de Execução da função Merge Sort é: " + tempoExecucaoMerge);
 
         //TODO MONTAR RELATORIO E TESTAR OS ARQUIVOS
     }
 
-/*    public static String retornaTempo(long millisSegundosExecucao){
+    public static String retornaTempo(long millisSegundosExecucao){
         int segundo = (int) (millisSegundosExecucao / 1000) % 60;
         int minutos = (int) (millisSegundosExecucao / 60000) % 60;
         int horas = (int) (millisSegundosExecucao/ 3600000);
 
         return String.format("%03d:%02d:%02d", horas, minutos, segundo);
-    }*/
+    }
 }
